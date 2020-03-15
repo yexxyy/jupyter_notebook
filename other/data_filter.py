3ef592a31a328785017bd6842b0ab6ca24d2415b
yetongxue <yexy@knownsec.com>
2020-03-15 22:33:11 +0800

    v3

--- other/data_filter.py
+++ other/data_filter.py
@@ -6,12 +6,11 @@ read_file_paths = [
     '/Users/yetongxue/Downloads/v4.csv',
 ]
 statistic_file_path = '/Users/yetongxue/Downloads/statistic.csv'
-statistic_column_names = ['Test_Items', 'Upper Limit', 'Lower Limit', 'Upper_Limit_NG', 'Lower_Limit_NG']
+statistic_column_names = ['Test_Items', 'Upper Limit', 'Lower Limit']
 ng_detail_column_names = ['SerialNumber', 'Test_Items_Data', 'Value', 'Upper Limit', 'Lower Limit']
 ng_detail_file_path = '/Users/yetongxue/Downloads/ng_detail.csv'
 start = '2020/2/10 01:03'
 end = '2020/4/13 01:03'
-config = 'BE7'
 time_fmt = '%Y/%m/%d %H:%M'
 
 start_time = datetime.datetime.strptime(start, time_fmt)
@@ -24,8 +23,9 @@ upper_ng_dict = {}  # 统计检测项目超过上限数量
 lower_ng_dict = {}  # 统计检测项目超过下限数量
 name_limit_map_upper = {}  # 检测项目名称与上限值对应关系
 name_limit_map_lower = {}  # 检测项目名称与下限值对应关系
-total_input = 0  # 记录符合条件（start_time, end_time, config）的输入记录条数
-ng_count = 0  # Test Pass/Fail Status == FAIL
+total_inputs = {}  # 记录符合条件（start_time, end_time）的输入记录条数
+ng_counts = {}  # Test Pass/Fail Status == FAIL
+configs = set()
 
 ng_write_f = open(ng_detail_file_path, 'w')
 ng_writer = csv.writer(ng_write_f)
@@ -40,8 +40,9 @@ def handle(reader):
     global lower_ng_dict
     global name_limit_map_upper
     global name_limit_map_lower
-    global total_input
-    global ng_count
+    global total_inputs
+    global ng_counts
+    global configs
 
     for i, line in enumerate(reader):
         if i == 0:
@@ -64,18 +65,21 @@ def handle(reader):
             if line_end and line_start:
                 LCM_FULL_SN = line[4]
                 LCM_FULL_SN_arr = LCM_FULL_SN.split('+')
-                if line_start > start_time and line_end < end_time and LCM_FULL_SN_arr[1][-3:] == config:
-                    total_input += 1
+                config = LCM_FULL_SN_arr[1][-3:]
+                configs.add(config)
+                if line_start > start_time and line_end < end_time:
+                    total_inputs.update({config: total_inputs.get(config, 0) + 1})
                     if line[2] == 'FAIL':
-                        ng_count += 1
+                        ng_counts.update({config: ng_counts.get(config, 0) + 1})
 
                     for index, column_name in enumerate(column_names):
                         if index >= 6:
                             # 统计超过上下限的项目
+                            column_name_key = '{}_{}'.format(column_names[index], config)
                             try:
                                 if float(upper_limts[index]) < float(line[index]):
                                     upper_ng_dict.update(
-                                        {column_names[index]: upper_ng_dict.get(column_names[index], 0) + 1})
+                                        {column_name_key: upper_ng_dict.get(column_name_key, 0) + 1})
                                     ng_writer.writerow(
                                         [line[5], column_name, line[index], upper_limts[index], lower_limts[index]])
                             except:
@@ -84,7 +88,7 @@ def handle(reader):
                             try:
                                 if float(lower_limts[index]) > float(line[index]):
                                     lower_ng_dict.update(
-                                        {column_names[index]: lower_ng_dict.get(column_names[index], 0) + 1})
+                                        {column_name_key: lower_ng_dict.get(column_name_key, 0) + 1})
                                     ng_writer.writerow(
                                         [line[5], column_name, line[index], upper_limts[index], lower_limts[index]])
                             except:
@@ -97,19 +101,29 @@ for path in read_file_paths:
     handle(reader)
 
 out_data = []
-for name in column_names:
-    if upper_ng_dict.get(name) or lower_ng_dict.get(name):
-        out_data.append([name, name_limit_map_upper.get(name), name_limit_map_lower.get(name),
-                         upper_ng_dict.get(name, 0), lower_ng_dict.get(name, 0)])
+configs = list(configs)
+for name in column_names[6:]:
+    upper_limit = name_limit_map_upper.get(name)
+    lower_limit = name_limit_map_lower.get(name)
+    row = [name, upper_limit, lower_limit]
+    for config in configs:
+        name_config_key = '{}_{}'.format(name, config)
+        row.extend([upper_ng_dict.get(name_config_key, 0) if upper_limit != 'NA' else 0, lower_ng_dict.get(name_config_key, 0) if lower_limit != 'NA' else 0])
+    out_data.append(row)
 
 with open(statistic_file_path, 'w') as f:
-    # 写入概览信息
     statistic_writer = csv.writer(f)
-    statistic_writer.writerow(['Total_Input', 'OK', 'NG', 'Config'])
-    statistic_writer.writerow([total_input, total_input - ng_count, ng_count, config])
+    # 写入column name, total input, ng count
+    total_inputs_row = ['Total Input'] + [''] * (len(statistic_column_names) - 1)
+    ng_counts_row = ['NG'] + [''] * (len(statistic_column_names) - 1)
+    upper_lower_limit_ng = [''] * len(statistic_column_names) + ['Upper Limit_NG', 'Lower Limit_NG'] * len(configs)
+    for config in configs:
+        statistic_column_names.extend([config, ''])
+        total_inputs_row.extend([total_inputs.get(config, 0), ''])
+        ng_counts_row.extend([ng_counts.get(config, 0), ''])
+    statistic_writer.writerows([statistic_column_names, total_inputs_row, ng_counts_row, upper_lower_limit_ng])
 
     # 写入检测项目NG个数
-    statistic_writer.writerows([['', ''], ['', ''], statistic_column_names])
     statistic_writer.writerows(out_data)
 
 read_f.close()
